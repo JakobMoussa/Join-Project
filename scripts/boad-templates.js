4cd666dfcc1e0b2dabaf7a3f05ff3ef71afd5e55
HenningOtte <HenningOtte91@gmail.com>
2025-07-13 21:43:47 +0200

    update add-task.css for layout and design improvements

--- scripts/boad-templates.js
+++ scripts/boad-templates.js
@@ -26,12 +26,6 @@ function createProgressTemplate(subtasks, numerus, subtaskDone) {
   `;
 }
 
-<<<<<<< HEAD
-function createPersonTemplate(username) {
-  return `<span class="avatar">${username}</span>`;
-}
-=======
 function createPersonTemplate(userObj, username) {
   return `<span class="avatar" style="background: ${userObj.color};">${username}</span>`;
 }
->>>>>>> c8caca961755ab40cfd200d93b89f57c13db19fa
